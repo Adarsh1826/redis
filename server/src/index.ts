19109193c2adc5d169c7d4bce3325cb948b892c7
adarsh singh <adsredmi182615@gmail.com>
2025-05-11 13:41:32 +0530

    added client

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,31 @@
-console.log(1);
+import express, { json } from "express";
+import { createClient } from "redis";
+const client = createClient()
+const app = express()
+app.use(express.json())
+app.post('/submit',async (req:any , res:any)=>{
+    const {problemId ,userId , code , langugae} = req.body
+    // push it into primary database
+
+    // now push it into redis
+
+    await client.lPush("submission",JSON.stringify({problemId,userId,code,langugae}))
+    res.json({
+        "message":"pushed successfullly"
+    })
+})
+async function startServer() {
+    try {
+        await client.connect()
+        console.log("Connected to redis");
+        app.listen(3000,()=>{
+            console.log("server is up");
+        })
+        
+        
+    } catch (error) {
+        console.error(error)
+    }
+    
+}
+startServer()
